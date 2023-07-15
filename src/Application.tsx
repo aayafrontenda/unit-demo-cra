b1b84af1c826505da7cb092ab9c8dd4adef8cc65
aayafrontenda <eeaayeyeah@gmail.com>
2023-07-15 19:39:12 +0600

    test: test1

--- src/Application.tsx
+++ src/Application.tsx
@@ -12,7 +12,7 @@ export const Application: FC = () => {
     <div>
       <nav>
         <Link data-testid="link-home" to="/">
-          Home1
+          Home
         </Link>
         <Link to="/about">About</Link>
       </nav>
