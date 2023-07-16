64591b19566aa14e9ee730c88e74568ddefc38b2
aayafrontenda <eeaayeyeah@gmail.com>
2023-07-16 08:07:16 +0600

    ci: v2.5.9

--- src/Application.tsx
+++ src/Application.tsx
@@ -12,7 +12,7 @@ export const Application: FC = () => {
     <div>
       <nav>
         <Link data-testid="link-home" to="/">
-          Home23451
+          Home234511
         </Link>
         <Link to="/about">About</Link>
       </nav>
