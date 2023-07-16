b5f03cb0420cdd29a50b66faca0ac249310c974c
aayafrontenda <eeaayeyeah@gmail.com>
2023-07-16 17:55:01 +0600

    fix: home fix

--- src/Application.tsx
+++ src/Application.tsx
@@ -12,7 +12,7 @@ export const Application: FC = () => {
     <div>
       <nav>
         <Link data-testid="link-home" to="/">
-          Home133311313000
+          Home
         </Link>
         <Link to="/about">About</Link>
       </nav>
